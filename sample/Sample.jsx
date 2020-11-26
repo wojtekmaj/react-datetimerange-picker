4ccbf9916398af74897f94b0b6ee7207471ab4be
Wojciech Maj <kontakt@wojtekmaj.pl>
2020-11-26 10:44:51 +0100

    Update sample

--- sample/Sample.jsx
+++ sample/Sample.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker';
 
 import './Sample.less';
@@ -7,30 +7,22 @@ const now = new Date();
 const yesterdayBegin = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1);
 const todayNoon = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12);
 
-export default class Sample extends Component {
-  state = {
-    value: [yesterdayBegin, todayNoon],
-  }
+export default function Sample() {
+  const [value, onChange] = useState([yesterdayBegin, todayNoon]);
 
-  onChange = (value) => this.setState({ value })
-
-  render() {
-    const { value } = this.state;
-
-    return (
-      <div className="Sample">
-        <header>
-          <h1>react-datetimerange-picker sample page</h1>
-        </header>
-        <div className="Sample__container">
-          <main className="Sample__container__content">
-            <DateTimeRangePicker
-              onChange={this.onChange}
-              value={value}
-            />
-          </main>
-        </div>
+  return (
+    <div className="Sample">
+      <header>
+        <h1>react-datetimerange-picker sample page</h1>
+      </header>
+      <div className="Sample__container">
+        <main className="Sample__container__content">
+          <DateTimeRangePicker
+            onChange={onChange}
+            value={value}
+          />
+        </main>
       </div>
-    );
-  }
+    </div>
+  );
 }
