30fe7fbfd9ad2f8a009b477712703c0298adcd51
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-06-05 22:40:53 +0200

    Simplify component props type export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import DateTimeRangePicker from './DateTimeRangePicker';
 
-import type { DateTimeRangePickerProps } from './DateTimeRangePicker';
+export type { DateTimeRangePickerProps } from './DateTimeRangePicker';
 
 export { DateTimeRangePicker };
-
-export type { DateTimeRangePickerProps };
 
 export default DateTimeRangePicker;
