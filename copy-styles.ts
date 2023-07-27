704f24a91e6b4eacc4b84f7f57d928428ab2bc87
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-07-27 12:30:55 +0200

    Replace custom script in copy-styles script with cpy

--- copy-styles.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-
-fs.copyFile('src/DateTimeRangePicker.css', 'dist/DateTimeRangePicker.css', (error) => {
-  if (error) {
-    throw error;
-  }
-  // eslint-disable-next-line no-console
-  console.log('DateTimeRangePicker.css copied successfully.');
-});
