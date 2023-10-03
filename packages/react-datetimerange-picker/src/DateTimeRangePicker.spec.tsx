5bf0433c9af4917c6b62e0324095ec4181b79eb5
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-10-03 11:27:57 +0200

    Fix unit test that used to be skipped because of JSDom issue

--- packages/react-datetimerange-picker/src/DateTimeRangePicker.spec.tsx
+++ packages/react-datetimerange-picker/src/DateTimeRangePicker.spec.tsx
@@ -40,14 +40,13 @@ describe('DateTimeRangePicker', () => {
     expect(nativeInputs[1]).toHaveAttribute('name', `${name}_to`);
   });
 
-  // See https://github.com/jsdom/jsdom/issues/3041
-  it.skip('passes autoFocus flag to first DateTimeInput component', () => {
+  it('passes autoFocus flag to first DateTimeInput component', () => {
     // eslint-disable-next-line jsx-a11y/no-autofocus
     const { container } = render(<DateTimeRangePicker autoFocus />);
 
     const customInputs = container.querySelectorAll('input[data-input]');
 
-    expect(customInputs[0]).toHaveAttribute('autofocus');
+    expect(customInputs[0]).toHaveFocus();
   });
 
   it('passes disabled flag to DateTimeInput components', () => {
